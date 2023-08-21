79af933306f14333d6611c2789940b65d4abea9b
alekappe <alecappellari00@gmail.com>
2023-08-21 11:23:14 +0200

    upload

--- website/auth.py
+++ website/auth.py
@@ -8,7 +8,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/index', methods=['GET', 'POST'])
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -25,7 +25,7 @@ def login():
         else:
             flash('Email does not exist.', category='error')
 
-    return render_template("index.html", user=current_user)
+    return render_template("login.html", user=current_user)
 
 
 @auth.route('/logout')
