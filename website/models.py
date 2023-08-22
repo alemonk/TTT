86f33ab9bd47aabecb0edbef512e67f1a03ab44a
alekappe <alecappellari00@gmail.com>
2023-08-22 13:55:29 +0200

    upload

--- website/models.py
+++ website/models.py
@@ -3,18 +3,40 @@ from flask_login import UserMixin
 from sqlalchemy import func
 
 
+# The Note class represents a user's note in the database.
 class Note(db.Model):
+    # The id attribute is the primary key for the Note table.
     id = db.Column(db.Integer, primary_key=True)
+
+    # The title attribute stores the title of the note.
     title = db.Column(db.String(150))
+
+    # The data attribute stores the original file data of the note.
     data = db.Column(db.LargeBinary)
-    content = db.Column(db.String(100000))
+
+    # The content attribute stores the extracted text from the note file.
+    content = db.Column(db.String(10000))
+
+    # The date attribute stores the date and time when the note was created.
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    # The user_id attribute is a foreign key that references the id of the user who created the note.
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
+# The User class represents a user account in the database.
 class User(db.Model, UserMixin):
+    # The id attribute is the primary key for the User table.
     id = db.Column(db.Integer, primary_key=True)
+
+    # The email attribute stores the email address of the user.
     email = db.Column(db.String(150), unique=True)
+
+    # The password attribute stores the password of the user.
     password = db.Column(db.String(150))
+
+    # The username attribute stores the username of the user.
     username = db.Column(db.String(150))
+
+    # The notes attribute is a relationship that represents all notes created by this user.
     notes = db.relationship('Note')
--- website/views.py
+++ website/views.py
@@ -1,3 +1,17 @@
+"""
+README
+
+This is a Flask application that allows users to create, view, and delete notes. Users can upload notes in the form of PDF or text files. The uploaded file data is stored in a `Note` object, which has a `content` attribute that stores the extracted text from the file. The application also provides routes for viewing and deleting notes.
+
+The application uses Flask blueprints to organize its views and routes. The `views` blueprint defines routes for creating, viewing, and deleting notes, as well as routes for other features such as a quiz and a personalized test.
+
+The application uses the Flask-Login extension to handle user authentication and session management. Users must be logged in to access the routes defined in the `views` blueprint.
+
+The application uses SQLAlchemy as its ORM and defines two models: `Note` and `User`. The `Note` model represents a user's note, while the `User` model represents a user account.
+
+The application also uses the PyPDF2 library to extract text from PDF files.
+"""
+
 import io
 import PyPDF2
 from flask import Blueprint, render_template, request, flash, jsonify
@@ -9,28 +23,34 @@ import json
 views = Blueprint('views', __name__)
 
 
+# Route for creating and viewing notes
 @views.route('/notes', methods=['GET', 'POST'])
 @login_required
 def create_note():
     print('Notes route called')
     if request.method == 'POST':
+        # Get note title and file data from form
         title = request.form.get('title')
         data = request.files['data'].read()
 
-        if len(data) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            content = unpack_file(data)
-            new_note = Note(title=title, data=data, content=content, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
-            print('Note: ' + title + ' added!')
-            return jsonify({'id': new_note.id, 'title': title})
+        # Extract content from file data
+        content = unpack_file(data)
 
+        # Create new note object and add it to the database
+        new_note = Note(title=title, data=data, content=content, user_id=current_user.id)
+        db.session.add(new_note)
+        db.session.commit()
+
+        # Flash success message and return JSON response
+        flash('Note added!', category='success')
+        print('Note: ' + title + ' added!')
+        return jsonify({'id': new_note.id, 'title': title})
+
+    # Render notes template on GET request
     return render_template("notes.html", user=current_user)
 
 
+# Helper function to extract content from file data
 def unpack_file(data):
     # Convert data to byte string if necessary
     if isinstance(data, str):
@@ -55,33 +75,49 @@ def unpack_file(data):
     elif file_type == 'txt':
         # Extract text from txt file
         content = data.decode()
-
     return content
 
 
+# Route for getting note content
 @views.route('/note-content', methods=['POST'])
 @login_required
 def get_note_content():
+    # Get note ID from form data
     note_id = request.form.get('note_id')
+
+    # Query database for note with given ID
     note = Note.query.get(note_id)
-    print(note.content)
+
+    # Check if note exists and belongs to current user
     if not note or note.user_id != current_user.id:
         return jsonify({'error': 'Note not found'})
+
+    # Return note content as JSON response
     return note.content
 
 
+# Route for deleting a note
 @views.route('/delete-note', methods=['POST'])
 @login_required
 def delete_note():
+    # Get note ID from request data
     note_id = json.loads(request.data)['noteId']
+
+    # Query database for note with given ID
     note = Note.query.get(note_id)
+
+    # Check if note exists and belongs to current user
     if note and note.user_id == current_user.id:
+        # Delete note from database and return success response
         db.session.delete(note)
         db.session.commit()
         return jsonify({'success': True})
+
+    # Return failure response if note does not exist or does not belong to current user
     return jsonify({'success': False})
 
 
+# Route for quiz feature (not implemented)
 @views.route('/quiz_me')
 @login_required
 def quiz_me():
@@ -89,6 +125,7 @@ def quiz_me():
     return render_template("quiz_me.html", user=current_user)
 
 
+# Route for personalized test feature (not implemented)
 @views.route('/personalized_test')
 @login_required
 def personalized_test():
